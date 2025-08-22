40575d8f0f5027d3a94902f39b24c96d3c92622e
Henry Cao <henryhrcao@gmail.com>
2025-08-22 18:02:37 -0400

    pages initializaiton

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import *
+
 from blocks import *
 import os
 import shutil
+import sys
 def copy(src,dest):
     if os.path.exists(dest):
         shutil.rmtree(dest)
@@ -16,7 +17,7 @@ def copy(src,dest):
             copy(filePath,destPath)
     return
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base):
     markdownFile = open(from_path)
     markdown = markdownFile.read()
     markdownFile.close()
@@ -26,7 +27,10 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     html = markdown_to_html_node(markdown)
     html = html.to_html()
-
+    title = title.replace('href="/', f'href="{base}')
+    title = title.replace('src="/', f'src="{base}')
+    html = html.replace('href="/', f'href="{base}')
+    html = html.replace('src="/', f'src="{base}')
     destFile = open(dest_path, "w")
     for line in templateLines:
         formatted = line.strip()
@@ -40,20 +44,23 @@ def generate_page(from_path, template_path, dest_path):
     destFile.close()
     return
 
-def generateWebsite(from_path, template_path, dest_path):
+def generateWebsite(from_path, template_path, dest_path, base):
     if os.path.isdir(from_path):
         files = os.listdir(from_path)
         for file in files:
             filePath = os.path.join(from_path,file)
             destPath = os.path.join(dest_path,file)
-            generateWebsite(filePath,template_path,destPath)
+            generateWebsite(filePath,template_path,destPath,base)
     else:
         dest_path = dest_path.replace(".md", ".html")
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-        generate_page(from_path, template_path, dest_path)
+        generate_page(from_path, template_path, dest_path,base)
         
 def main():
-    copy("static","public")
+    basePath = "/"
+    if len(sys.argv) ==2:
+        basePath = sys.argv[1]
+    copy("static","docs")
     #generate_page("content/index.md", "template.html","public/index.html")
-    generateWebsite("content", "template.html","public")
+    generateWebsite("content", "template.html","docs", basePath)
 main()
